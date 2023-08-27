f13090345276476e0ffa441121b60a560725d0e2
Yantasticc <yashkalamkhede2002@gmail.com>
2023-08-27 13:13:50 +0530

    intial commit

---
+++ src/timeline/Timeline.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Timeline.css";
+import Suggestions from './Suggestions';
+
+function Timeline() {
+  return (
+    <div className='timeline'>
+
+        <div className='timeline__left'>timeline</div>
+        <div className='timeline__right'>
+            <Suggestions />
+        </div>
+
+    </div>
+  )
+}
+
+export default Timeline
